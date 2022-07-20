353b966cbbef75581fff4c852ad1e6a91294f429
ajay-2019011 <ajaysheoran2019011@gmail.com>
2022-07-20 23:51:32 +0530

    Created React-App for Airbnb Clone

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import logo192 from '../images/airbnb-logo.png'
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <img id="logo" src={logo192} alt="Not found"/>
+    </div>
+  )
+}
