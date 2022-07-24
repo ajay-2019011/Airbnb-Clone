47ebdc4bf2be52b79e7909f3f70afefa45bf73e1
ajay-2019011 <ajaysheoran2019011@gmail.com>
2022-07-24 21:48:24 +0530

    Props used to make a template for similar elements

--- src/App.js
+++ src/App.js
@@ -9,13 +9,36 @@ export default function App() {
     <div>
       <Navbar />
       <Hero />
-      <Card
-        //require() to send the image locally
-        img={require("./images/katie-zaferes.png")}
-        line1={[" 5.0", " (6) USA"]}
-        line2="Life Lessons with Katie Zaferes"
-        line3={["From $136 ", "/ person"]}
-      />
+      <div className="cards">
+        <Card
+          //require() to send the image locally
+          img={require("./images/katie-zaferes.png")}
+          line1={[" 5.0", " (6) USA"]}
+          line2="Life Lessons with Katie Zaferes"
+          line3={["From $136 ", "/ person"]}
+        />
+        <Card
+          //require() to send the image locally
+          img={require("./images/wedding.png")}
+          line1={[" 5.0", " (30) USA"]}
+          line2="Learning wedding photography"
+          line3={["From $125 ", "/ person"]}
+        />
+        <Card
+          //require() to send the image locally
+          img={require("./images/mountains.jpeg")}
+          line1={[" 4.8", " (2) USA"]}
+          line2="Group Mountain Biking"
+          line3={["From $50 ", "/ person"]}
+        />
+        <Card
+          //require() to send the image locally
+          img={require("./images/boxings.jpg")}
+          line1={[" 4.9", " (29) USA"]}
+          line2="Mike Tyson Academy"
+          line3={["From $50 ", "/ person"]}
+        />
+      </div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import Star from '../images/star.png'
 export default function Card(props) {
   return (
     <div>
-      <img id="katie" src={props.img} alt="Not Found" />
+      <img id="images" src={props.img} alt="Not Found" />
       <div id="card-content">  
         <img id="star" src={Star} alt="Not Found" />
         <span>
