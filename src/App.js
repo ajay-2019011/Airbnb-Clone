d803a6f2f330a9c36f7febd1e99183cc412a2912
ajay-2019011 <ajaysheoran2019011@gmail.com>
2022-07-24 18:52:26 +0530

    Using Props to make reusable components (PS: require() func to take images locally)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-// import logo from './logo.svg';
-//import './App.css';
+
 import React from "react"
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <Card
+        //require() to send the image locally
+        img={require("./images/katie-zaferes.png")}
+        line1={[" 5.0", " (6) USA"]}
+        line2="Life Lessons with Katie Zaferes"
+        line3={["From $136 ", "/ person"]}
+      />
     </div>
   );
 }
