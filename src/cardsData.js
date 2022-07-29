60c055fdb96d9eac236f5d35f335a96a7a498f0e
ajay-2019011 <ajaysheoran2019011@gmail.com>
2022-07-30 00:18:59 +0530

    created cardsData.js file to export data as an array of objects

---
+++ src/cardsData.js
@@ -0,0 +1,28 @@
+// exporting array of objects by using
+export default [
+  {
+    //require() to send the image locally
+    img: require("./images/katie-zaferes.png"),
+    line1: [" 5.0", " (6) USA"],
+    line2: "Life Lessons with Katie Zaferes",
+    line3: ["From $136 ", "/ person"]
+  },
+  {
+    img: require("./images/wedding.png"),
+    line1: [" 5.0", " (30) USA"],
+    line2: "Learning wedding photography",
+    line3: ["From $125 ", "/ person"]
+  },
+  {
+    img: require("./images/mountains.jpeg"),
+    line1:  [" 4.8", " (2) USA"],
+    line2: "Group Mountain Biking",
+    line3: ["From $50 ", "/ person"]
+  },
+  {
+    img: require("./images/boxings.jpg"),
+    line1: [" 4.9", " (29) USA"],
+    line2: "Mike Tyson Academy",
+    line3: ["From $50 ", "/ person"]
+  }
+]
