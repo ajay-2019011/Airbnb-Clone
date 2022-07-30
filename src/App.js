8484a8da5174bce322aaf30e3ae72135b71235cc
ajay-2019011 <ajaysheoran2019011@gmail.com>
2022-07-30 22:48:41 +0530

    Added a footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import Footer from "./components/Footer"
 import cardsData from "./cardsData"
 
 export default function App() {
@@ -22,6 +23,7 @@ export default function App() {
       <div className="cards">
         {cardsElements}
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Footer(){
+  return (
+    <div className="footer">
+        <h3> Copyrights@Airbnb 2022</h3>
+    </div>
+  )
+}
